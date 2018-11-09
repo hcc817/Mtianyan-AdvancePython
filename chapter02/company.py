55f95ed3876a9ddf7f4638f18de03af81689c54a
mtianyan <1147727180@qq.com>
2018-11-09 13:28:17 +0800

    学习了第三章魔法函数，str repr getitem len。了解了len原生支持

--- chapter02/company.py
+++ chapter02/company.py
@@ -1,21 +1,29 @@
 class Company(object):
     def __init__(self, employee_list):
         self.employee = employee_list
-    #
-    # def __getitem__(self, item):
-    #     return self.employee[item]
+
+    def __getitem__(self, item):
+        """任意一个类都可以添加魔法函数，返回item位置的值"""
+        return self.employee[item]
 
     def __len__(self):
         return len(self.employee)
 
+    def __str__(self):
+        """打印时"""
+        return ",".join(self.employee)
+
+    def __repr__(self):
+        """shell中"""
+        return "shell: " + ",".join(self.employee)
 
 
 company = Company(["tom", "bob", "jane"])
 
+company1 = company[:2]
 
-# company1= company[:2]
-#
-# print(len(company))
-#
-# for em in company1:
-#     print(em)
+print(len(company1))
+for em in company1:
+    print(em)
+print(len(company))
+print(company)
